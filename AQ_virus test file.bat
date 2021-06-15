d6f72c231cbd3492c20610f4b80cfb4f75c0631f
Ashfaaq Rifath <85920545+ashfaaq2002@users.noreply.github.com>
2021-06-15 17:39:24 +0530

    Add files via upload

---
+++ AQ_virus test file.bat
@@ -0,0 +1,16 @@
+@echo off
+color 6
+echo Dominant software initiated
+echo Disinged by Ashfaaq 
+echo Matrix virus uploading
+echo System compromization will begin shortly
+echo .....
+echo virus downloaded
+echo Your system has been compromized
+pause
+goto matrix
+:matrix
+color 2
+:start
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto start 
